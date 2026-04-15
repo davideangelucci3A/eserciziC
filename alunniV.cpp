4b4fff3bd0b9397a1bfaf8b944eff6d83115c60a
davideangelucci3A <stud.angeluccid@gdromagnosi.edu.it>
2026-04-15 08:41:35 +0000

    es corretti

--- alunniV.cpp
+++ alunniV.cpp
@@ -8,11 +8,12 @@ using namespace std;
 int main() { 
     int n=10, contam=0, ins=0;
     int vet[n];
+    int ins1[n];
     double m[n];
     for(int i=0;i<n;i++){
          vet[i]=i+1;
             cout<<"inserire il totale dei voti dell'alunno "<<vet[i]<<": ";
-            int x;
+            int x, ins2=0;
             double media=0, somma=0;
             cin>>x;
             int voti[x];
@@ -20,8 +21,10 @@ int main() {
                 cout<<"inserire il voto ";
                 cin>>voti[j];
              somma=voti[j]+somma;
-             if(voti[j]<6){ ins++;}
+             if(voti[j]<6){ ins++;
+            ins2++;}
             }
+            ins1[i]=ins2;
             media=somma/x;
             if(media<6){contam++;}
             m[i]=media;
@@ -29,7 +32,7 @@ int main() {
             
     }
     for(int i=0;i<n;i++){
-        cout<<"alunno numero "<<vet[i]<<"|media: "<<m[i]<<endl;
+        cout<<"alunno numero "<<vet[i]<<"|media: "<<m[i]<<"| numero insufficienze "<<ins1[i]<<endl;
 
     }
     cout<<"il numero di alunni con una media inferiore al 6 è "<<contam<<" e il totale delle insufficienze è "<<ins;
